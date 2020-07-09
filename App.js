36e3e3ae47a8ad23003b9fb4052500adf1f032e9
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-07-09 10:39:24 +0700

    home screen base added

--- App.js
+++ App.js
@@ -1,71 +1,31 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { tailwind } from "tailwind";
-import TicTacToe from "./components/TicTacToe";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  turnText: {
-    fontSize: 30,
-    fontWeight: "bold",
-    textAlign: "center",
-    color: "#aaaaaa",
-  },
-  tttContainer: {
-    marginVertical: 20,
-  },
-});
+const Stack = createStackNavigator();
+
+const routes = [
+  { name: "Home", screen: require("./screens/HomeScreen").default },
+  { name: "Game", screen: require("./screens/GameScreen").default },
+];
 
 const App = () => {
-  const [turn, setTurn] = useState(1);
-  const [gameStarted, setGameStarted] = useState(false);
-  const [winner, setWinner] = useState(null);
-  const [alert, setAlert] = useState(null);
-  const [size, setSize] = useState(4);
-
-  const toggleTurn = () => {
-    setTurn((turn % 2) + 1);
-  };
-
-  const resetGameState = () => {
-    setGameStarted(false);
-    setWinner(0);
-    setTurn(1);
-  };
-
-  let alertBox = null;
-  if (alert) {
-    alertBox = (
-      <Text
-        style={tailwind("text-lg font-bold")}
-        onPress={() => setAlert(null)}
-      >
-        {alert}
-      </Text>
-    );
-  }
-
   return (
-    <View style={tailwind("flex-1 bg-white p-6 py-12")}>
-      <StatusBar style="auto" backgroundColor="#eee" />
-      <Text style={styles.turnText}>Player {turn} Move</Text>
-      <View style={styles.tttContainer}>
-        <TicTacToe
-          turn={turn}
-          onChangeTurn={toggleTurn}
-          resetGameState={resetGameState}
-          handleSetWinner={(player) => setWinner(player)}
-          addAlert={(message) => setAlert(message)}
-          size={size}
-        />
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {routes.map((route) => {
+          return (
+            <Stack.Screen
+              key={route.name}
+              name={route.name}
+              component={route.screen}
+              options={{ headerShown: false }}
+            />
+          );
+        })}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
